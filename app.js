0389fb412d3b32d0947bf0bd11ae5dd1b62d7cd2
PabloV <pablovalentin09@gmail.com>
2021-05-16 11:53:56 -0300

    nuevas funcionalidades

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ app.listen(3000, () =>{
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
