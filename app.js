33c888c70624e2e22622ce9b34c9c0f10f9fe365
PabloV <pablovalentin09@gmail.com>
2021-05-17 16:34:08 -0300

    agregando paginas

--- app.js
+++ app.js
@@ -17,3 +17,7 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
