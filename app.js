ede31df3f4e305497bc3d3d15563b9abd35b5fab
PabloV <pablovalentin09@gmail.com>
2021-05-18 21:53:34 -0300

    configurando Heroku

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 3000
 
 const app = express();
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log("Servidor corriendo en el puerto 3000");
 });
 
